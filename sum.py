42848f06c0f91a673175f0c0ecaada379068e047
Pawan Bhatt <84166251+Pawan-Bhatt@users.noreply.github.com>
2022-03-11 23:59:43 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,5 @@
+def sum(first_number , second_number):
+    Final_sum = first_number + second_number
+    return Final_sum
+
+print(sum(3 , 4))     # this will print the sum of 3 and 4 which is equal to 7
